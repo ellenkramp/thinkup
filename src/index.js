cc1b562f045493d91d547aac10b9d8fe97018433
Jaehee Kim <jaeheekim051510@gmail.com>
2018-05-10 13:58:01 -0400

    router fixed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import CreateIdea from './Components/CreateIdea';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
