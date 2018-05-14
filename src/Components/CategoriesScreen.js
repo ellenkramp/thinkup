ca33af16dee4ee1a5d83831b555a048c74c93723
Jaehee Kim <jaeheekim051510@gmail.com>
2018-05-14 11:09:02 -0400

    categories added

--- src/Components/CategoriesScreen.js
+++ src/Components/CategoriesScreen.js
@@ -3,6 +3,9 @@ import React from 'react';
 import logo from '../images/logo.png'
 import music from '../images/music.jpg';
 import design from '../images/design.jpg';
+import film from '../images/film.jpg';
+import gaming from '../images/gaming.jpg';
+
 
 import { Link } from "react-router-dom";
 
@@ -17,6 +20,8 @@ let CategoriesScreen = () =>
         <div className="categories">
             <div className="c"><Link to="/categories/:id"><img src={music} className="c-img" alt="music" /></Link></div>
             <div className="c"><Link to="/categories/:id"><img src={design} className="c-img" alt="design" /></Link></div>
+            <div className="c"><Link to="/categories/:id"><img src={film} className="c-img" alt="film" /></Link></div>
+            <div className="c"><Link to="/categories/:id"><img src={gaming} className="c-img" alt="gaming" /></Link></div>
         </div> 
     </div>
 
