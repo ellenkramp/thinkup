f161abb51a9f2524b4bcd83fda9e24d00f240820
Jaehee Kim <jaeheekim051510@gmail.com>
2018-05-14 12:05:39 -0400

    styling to createIdea Screen

--- src/Components/CreateIdea.js
+++ src/Components/CreateIdea.js
@@ -23,34 +23,55 @@ class CreateIdea extends Component {
             <div className='create-idea'>
                 <h1>Add Your Idea</h1>
                 <form onSubmit={this.handleSubmit}>
+                    <div className="ci">
                     <label>Post Title</label>
+                        <div>
                         <input
                         name="title"
                         placeholder="title"
                         type="text" />
-                    
+                        </div>
+                    </div>
+
+                    <div className="ci">
                     <label>Who Are You?</label>
+                        <div>
                         <input
                         name="your name"
                         type="text"
                         placeholder="what should we call you?" />
+                        </div>
+                    </div>
                     
-                    <label>Category</label>
+                        <div className="ci">
+                        <label>Category</label>
+                        <div className="select">
                         <select 
                         name="category">
                             <option value="none">None</option>
-                            <option value="crypto">Crypto</option>
+                            <option value="crypto">Gaming</option>
                             <option value="design">Design</option>
                             <option value="music">Music</option>
-                            <option value="food">Food</option>
+                            <option value="food">Film</option>
                         </select>
+                        </div>
+                        </div>
+                    
+
+                    <div className="ci">
                     <label>Your Idea</label>
+                    <div>
                         <textarea
                         name="idea"
-                        placeholder="type your idea here" />                
+                        placeholder="type your idea here" />  
+                    </div>
+                    </div>
+
+                    <div className="ci">              
                     <button type="submit" >
                         Post Idea
                     </button>
+                    </div>
                 </form>
             </div>
         ) 
